d3e64de41f4e3eee915972387b4a9ba9e767613f
Михаил <mike.milos@yandex.ru>
2018-11-29 15:57:55 +0300

    start

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <boost/program_options.hpp>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    std::cout << argv[0] << std::endl;
+    return 0;
+}
