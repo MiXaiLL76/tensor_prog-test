ba9c982f79ce24c1d35628e31f1ce6fdc20f35a5
MiXaiLL76 <mike.milos@yandex.ru>
2018-12-01 12:09:00 +0300

    edit done

--- src/FileTree.h
+++ src/FileTree.h
@@ -10,38 +10,38 @@ class FileTree
 {
   public:
     FileTree(
-		std::string name,						/* ��� ����� ��� ��������� */
-		std::string path,						/* ����, � ��������� �����*/
-		std::vector<std::string> *include_path,	/* ������ �� ����� � ����������� � ������� <> */
-		bool local_include = true,				/* ���� true �� ��������� �� ��, ��� ������ ������������� ����.*/
-		FileTree *parent = NULL					/* ������������ �����.*/
+		std::string name,						/* Имя файла для обработки */
+		std::string path,						/* Путь, к дефолтной папке*/
+		std::vector<std::string> *include_path,	/* Ссылка на папки с заголовками в формате <> */
+		bool local_include = true,				/* Если true то указывает на то, что искать относительные пути.*/
+		FileTree *parent = NULL					/* Родительский класс.*/
 	);
-    std::string file_name;						/* �������� �����. �������� ����� ��� ��������� � private,
-												������� ������ �� ������� getName(). ����� ������ ��� �� �������������*/
+    std::string file_name;						/* Название файла. возможно нужно его запихнуть в private,
+												сделать доступ из функции getName(). Чтобы просто так не редактировали*/
     std::string file_path;						/* */
 
     bool local_include;
-    bool exists;								/* ��������������� � ������������� �����. (���� � ������� �����)*/
-    bool is_loop;								/* ����� �������� TRUE, ���� ���� �������� � �����.*/
+    bool exists;								/* Свидетельствует о существовании файла. (тоже в функцию можно)*/
+    bool is_loop;								/* Имеет значение TRUE, если файл приведет к циклу.*/
 
-    void print(std::string sep = "", std::ofstream *out_stream = NULL);			/* ����������� ������� ������ (�������� ������� �� ������ �����,
-												�� ��� ��� �������� ����� ������� ����������.*/
-    std::map<std::string, int> files;			/* ������ ������. ������������� ������ 1 �����, �.�. ROOT*/
-    int build();								/* ������� ���������� �����*/
+    void print(std::string sep = "", std::ofstream *out_stream = NULL);			/* Рекурсивная функция вывода (рекурсия конечно не лучший выход,
+												но тут это наверное самая простая реализация.*/
+    std::map<std::string, int> files;			/* Список файлов. Используеться только 1 класс, т.е. ROOT*/
+    int build();								/* Функция построения древа*/
 
   private:
     std::vector<std::string> *include_path;		/**/
     FileTree *parent;							/**/
-    FileTree *_root;							/* ������ �� ����� ������ ������� � �����.*/
-    FileTree *getRoot();						/* ������� ��� ���������� ������� ��������.
-												����� ������� � ���������� ������ ����, 
-												�� � ������� ���.*/
+    FileTree *_root;							/* Ссылка на самый первый элеметн в древе.*/
+    FileTree *getRoot();						/* Функция для вычисления первого элеметна.
+												Можно конечно и передавать ссылку всем, 
+												но я захотел так.*/
     
-	bool isLoop();								/* ������� �������� �� ����������� �����������*/
-    std::vector<FileTree> children;				/* ������ �����.*/
+	bool isLoop();								/* Функция проверки на циклическую зависимость*/
+    std::vector<FileTree> children;				/* Список детей.*/
 };
 
-inline char separator()							/* ����������� ��� ����� � �������.*/
+inline char separator()							/* Разделители для винды и линукса.*/
 {
 #ifdef _WIN32
     return '\\';
